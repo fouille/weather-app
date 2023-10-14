d90ccb83d072c3fb62477a914b57b3804fe75f8c
Kravetskyi <nikita.kravecz@gmail.com>
2023-10-14 18:17:51 +0300

    changed HTTP to HTTPS in API url

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_KEY = "b2eb4160b71c4937b0d84320232009";
-export const URL = "http://api.weatherapi.com/v1/forecast.json?key=";
+export const URL = "https://api.weatherapi.com/v1/forecast.json?key=";
 export const TIMEOUT_SEC = 10;
 export const CURRENT_W = "&days=7&aqi=yes&alerts=no";
 export const SEVEN_DAY_F = "&days=7&aqi=yes&alerts=no";
